f1e74feb723b2ad4f57fe96f5918873427ce8635
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2023-10-23 22:34:43 +0530

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://harshkumarsingh005:Luckymongodbdjv2@djv2.mq6nkat.mongodb.net/test?retryWrites=true';
+secretOrKey= "harshissecret"
+module.exports = {
+    mongoURI: dbPassword,
+    secretOrKey: secretOrKey
+};
