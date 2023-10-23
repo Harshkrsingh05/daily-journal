94fb67c6b571a6a51f642ab4712c9d9de6561583
Harsh Kumar Singh <114430264+Harshkrsingh05@users.noreply.github.com>
2023-10-23 22:43:50 +0530

    Delete config/keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-dbPassword = 'mongodb+srv://harshkumarsingh005:Luckymongodbdjv2@djv2.mq6nkat.mongodb.net/test?retryWrites=true';
-secretOrKey= "harshissecret"
-module.exports = {
-    mongoURI: dbPassword,
-    secretOrKey: secretOrKey
-};
